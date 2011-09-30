118dc9866d3b12982f8126a2994166f1613d435b
jplethier <jplethier@gmail.com>
2011-09-30 12:35:30 -0300

    criacao correto do controlador do usuario com a criacao funcionando corretamente

---
+++ app/controllers/usuarios_controller.rb
@@ -0,0 +1,28 @@
+class UsuariosController < ApplicationController
+
+  def new
+    @usuario = Usuario.new
+  end
+
+  def create
+    @usuario = Usuario.new(params[:usuario])
+    if @usuario.save
+      render 'index'
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @usuarios = Usuario.all
+    debugger
+    1
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+end
---
+++ spec/views/usuarios/update.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "usuarios/update.html.erb" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
