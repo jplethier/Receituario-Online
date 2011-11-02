43dcb0490a95992dc4a76bb263b340beb002a167
jplethier <jplethier@gmail.com>
2011-11-02 17:08:10 -0200

    cadastro de medico pela clinica funcionando

--- app/controllers/medicos_controller.rb
+++ app/controllers/medicos_controller.rb
@@ -1,9 +1,17 @@
 class MedicosController < ApplicationController
   
   def new
+    @medico = Medico.new
+    @medico.build_usuario
   end
 
   def create
+    @medico = Medico.new(params[:medico])
+    if @medico.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
 end
--- app/models/medico.rb
+++ app/models/medico.rb
@@ -2,12 +2,14 @@ class Medico < ActiveRecord::Base
 
 	belongs_to :usuario
 
+  accepts_nested_attributes_for :usuario
+
   has_many :clinicas_medicos
   has_many :clinicas, :through => :clinicas_medicos
 
   validates :crm, :presence => true
 
-  attr_accessible :crm
+  attr_accessible :crm, :usuario_attributes
 
   def self.por_usuario(usuario)
     where("medicos.usuario_id = ?", usuario.id)
